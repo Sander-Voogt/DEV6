2f566348f44999ff70319a3c130475eaf5f3b09f
Dion <dionlieshout@me.com>
2016-11-30 14:07:38 +0100

    login route test

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+var login = require('./routes/login');
 //TODO: add all routes
 
 var app = express();
