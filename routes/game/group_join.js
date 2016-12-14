8d801a71de45ab02e42fb2a58a49a5a0f2e6e5aa
AndreHummel <andre.97@hotmail.com>
2016-12-14 12:07:27 +0100

    part of group managment page

--- routes/game/group_join.js
+++ routes/game/group_join.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('../../database.js');
 
-router.get('/', function(req, res, next) {
-    //TODO: return all groups available to join
-    res.render('group_join', { groups: groups });
+router.get('/', function (req, res) {
+    mysql.mysqlConnection.query('SELECT f_id, name, members, money FROM family', function(err, rows)
+    {
+        //TODO: return the group info: name and players
+        console.log(rows);
+        res.render('groups', {items: rows});
+    });
 });
 
 router.post('/', function(req, res, next) {
-    //TODO: handle submitted group to join, add player to group in database
+    mysql.mysqlConnection.query('SELECT f_id FROM family WHERE f_id="' + req.body.groupid + '"', function (err, results) {
+        var a = JSON.stringify(results);
+        console.log(a);
 
+        if (results == "") {
+            res.end("Voer een geldige family id in.");
+        } else {
+            //TODO: handle submitted group to join, add player to group in database
+        }
+    });
 });
 
 module.exports = router;
