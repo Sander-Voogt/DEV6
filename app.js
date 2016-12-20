c40a21766a2784c6758de4a665a8a3c3581c5aa1
AndreHummel <andre.97@hotmail.com>
2016-12-20 22:33:47 +0100

    group management v.0.1

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', require('./routes/index'));
 app.use('/register', require('./routes/register'));
 app.use('/login', require('./routes/login'));
-app.use('/groups', require('./routes/game/group_join'));
+app.use('/game/groups', require('./routes/game/group_join'));
 app.use('/logout', require('./routes/logout'));
 app.use('/game/crime', require('./routes/game/crimes'));
 app.use('/game/shop', require('./routes/game/shop'));
--- routes/game/group_join.js
+++ routes/game/group_join.js
@@ -3,23 +3,36 @@ var router = express.Router();
 var mysql = require('../../database.js');
 
 router.get('/', function (req, res) {
-    mysql.mysqlConnection.query('SELECT f_id, name, members, money FROM family', function(err, rows)
-    {
-        //TODO: return the group info: name and players
+    mysql.mysqlConnection.query('SELECT f_id, name, members, money FROM family', function(err, rows){
+        var username = req.cookies.username;
         console.log(rows);
-        res.render('groups', {items: rows});
+        res.render('groups', {items: rows, username: username});
     });
 });
 
 router.post('/', function(req, res, next) {
-    mysql.mysqlConnection.query('SELECT f_id FROM family WHERE f_id="' + req.body.groupid + '"', function (err, results) {
-        var a = JSON.stringify(results);
-        console.log(a);
-
-        if (results == "") {
-            res.end("Voer een geldige family id in.");
-        } else {
-            //TODO: handle submitted group to join, add player to group in database
+    var user = req.cookies.username;
+    mysql.mysqlConnection.query('SELECT f_id FROM users WHERE username="' + user + '"', function (err, results) {
+        if (results[0].f_id == "0") {
+            mysql.mysqlConnection.query('SELECT f_id FROM family WHERE f_id="' + req.body.groupid + '"', function (err, results) {
+                if (results == "") {
+                    res.end("Voer een geldige family id in.");
+                }
+                else {
+                    mysql.mysqlConnection.query('SELECT members FROM family WHERE f_id = "' + req.body.groupid + '"', function (err, results) {
+                        var members = (results[0].members);
+                        console.log("members seledted" + members);
+                        mysql.mysqlConnection.query('UPDATE users SET f_id = ? WHERE username = ?', [req.body.groupid, user], function (err, results) {
+                        });
+                        mysql.mysqlConnection.query('UPDATE family SET members = ? WHERE f_id = ?', [members + 1, req.body.groupid], function () {
+                            res.redirect(req.get('referer'))
+                        });
+                    });
+                }
+            });
+        }
+        else {
+            res.end("Je hebt al een family lul")
         }
     });
 });
