c73c4df4b7cbfd7e0724e4afe6859146bbd47371
Zino <imznqa@gmail.com>
2017-01-06 21:16:48 +0100

    leaderboard

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var mysql = require('../database.js');
 
 router.get('/', function(req, res, next) {
-    var _username = req.cookies.username;
+    var username = req.cookies.username;
     mysql.mysqlConnection.query('SELECT username,money FROM users ORDER BY money DESC LIMIT 3;', function(err, data) {
             res.render('leaderboard', {data: data });
-            console.log(data[2].username);
-            console.log(data[2].money);
+            console.log(data[0].username);
+            console.log(data[0].money);
         });
     });
 
