305f3326e0ad9cee29169357df4a02da7ab7b52b
Dion <dionlieshout@me.com>
2017-01-06 14:18:45 +0100

    v1.0

--- routes/game/inventory.js
+++ routes/game/inventory.js
@@ -24,4 +24,23 @@ router.get('/:weapon', function(req, res, next) {
     });
 });
 
+router.get('/sell/:weapon', function(req, res, next) {
+    var username = req.cookies.username;
+    var weaponID = req.params.weapon;
+
+    mysql.mysqlConnection.query('SELECT weapon, worth FROM inventory WHERE id="' + weaponID + '"', function(err, results) {
+        var weaponWorth = (results[0].worth);
+        var weaponName = (results[0].weapon);
+
+        mysql.mysqlConnection.query('UPDATE users SET weapon_id = NULL WHERE username = "' + username + '" AND weapon_id = "' + weaponID + '"', function(err, results) {});
+        mysql.mysqlConnection.query('UPDATE users SET users.money=users.money+' + weaponWorth +' WHERE username="' + username +'"', function(err, results) {});
+        mysql.mysqlConnection.query('UPDATE shop SET shop.amount=shop.amount+1 WHERE name="' + weaponName + '"', function(err, results) {});
+        mysql.mysqlConnection.query('DELETE FROM inventory WHERE id="' + weaponID + '"', function(err, results) {});
+
+        res.redirect("/game/inventory");
+        res.end();
+    });
+
+});
+
 module.exports = router;
