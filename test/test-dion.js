cd1404f4512b2994aa427fa0e9be2ab4035ae1e0
Dion <dionlieshout@me.com>
2017-01-06 14:18:57 +0100

    tests

---
+++ test/test-dion.js
@@ -0,0 +1,56 @@
+var supertest = require('supertest');
+var app = require('../app.js');
+var agent = supertest.agent(app);
+var expect  = require("chai").expect;
+
+describe('Tests Dion: winkel en inventaris', function () {
+    it('inloggen en cookie registeren', function(done) {
+        agent
+            .post('/login')
+            .type('form')
+            .send({username: 'Dion'})
+            .send({password: 'hoi'})
+            .expect(302)
+            .expect('Location', '/')
+            .expect('set-cookie', /username/)
+            .end(function(err, res) {
+                agent.jar.setCookie(res.headers['set-cookie'][0])
+                console.log("\nCookies: " + res.headers['set-cookie'] + "\n");
+                return done();
+            });
+    });
+
+    it("wapenwinkel openen, geld aflezen", function (done){
+        agent
+            .get('/game/shop')
+            .expect(200)
+            .then(function (res) {
+                expect(res.text).to.contains("Jouw geld:");
+                return done();
+            })
+            .catch(function (err) {
+                console.log(err.response.text);
+                return
+            });
+    });
+
+    it("wapen aanschaffen", function (done){
+        agent
+            .get('/game/shop/M9')
+            .expect(302, done);
+    });
+
+    it("inventaris controleren of wapen in bezit is", function (done){
+        agent
+            .get('/game/inventory')
+            .expect(200)
+            .then(function (res) {
+                expect(res.text).to.contains("<td>M9</td>");
+                return done();
+            })
+            .catch(function (err) {
+                console.log(err.response.text);
+                return
+            });
+    });
+});
