8a65c01a7d5f2828ca20ad73442a4a82acbe84d6
Dion <dionlieshout@me.com>
2017-03-24 13:26:43 +0100

    1e commit object cookie

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,12 @@ router.get('/', function(req, res, next) {
     //TODO: show a proper welcome screen with links to login or register
 
   var username = req.cookies.username;
+
+  if(req.cookies.data){
+    console.log(req.cookies.data);
+    //TODO: uitbreiden data uitlezen
+  }
+
   res.render('index', { title: 'Express', username: username });
 });
 
--- routes/login.js
+++ routes/login.js
@@ -21,9 +21,17 @@ router.post('/', function(req, res, next) {
             mysql.mysqlConnection.query('SELECT password FROM users WHERE username="' + req.body.username + '"', function (err, passwordtest) {
                 var passwordtest = JSON.stringify(passwordtest);
                 if (passwordtest == "[{"+'"'+"password"+'"'+":"+'"'+req.body.password+'"'+"}]"){
-                    res.cookie('username', req.body.username, { maxAge: 3600000 });
-                    res.redirect('/');
-                    res.end();
+
+                    //Handle data object in cookie
+                    mysql.mysqlConnection.query('SELECT u.id, u.username, u.money FROM users AS u WHERE username="' + req.body.username + '"', function (err, results) {
+                        console.log(results);
+                        res.cookie('data', results, { maxAge: 3600000 });
+
+                        res.cookie('username', req.body.username, { maxAge: 3600000 });
+                        res.redirect('/');
+                        res.end();
+                    });
+
                 } else {
                     res.end("Dit wachtwoord is helaas verkeerd.")
                 }
