5ede2fefd024f71377676c2d09c670f0631fa444
AndreHummel <andre.97@hotmail.com>
2017-04-18 22:45:21 +0200

    Opzetje voor unitTest

---
+++ test/GroupTest.js
@@ -0,0 +1,21 @@
+/**
+ * Created by andre on 18-4-2017.
+ */
+
+var app = require('../app.js');
+const Group = require('../group.js');
+
+describe("Test object Group", function() {
+    it('Create an instance without values', function(done) {
+
+    });
+    it('Create an instance with values', function(done) {
+
+    });
+    it('set()', function(done) {
+
+    });
+    it('get()', function(done) {
+
+    });
+});
